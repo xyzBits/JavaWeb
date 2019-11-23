7c51d58cde3bd7e55c6a98e6943957b495dd2769
lidf077 <lidf077@gmail.com>
2019-11-23 14:01:28 +0800

    使用maven管理项目

---
+++ src/main/java/com/dongfang/xml/TestXmlParser.java
@@ -0,0 +1,17 @@
+package com.dongfang.xml;
+
+
+import org.dom4j.io.SAXReader;
+import org.junit.jupiter.api.Test;
+
+public class TestXmlParser {
+    @Test
+    public void testXml1() {
+        // 1、导入dom解析包，dom4j-1.6
+        // 2、创建一个SAXReader，一个xml文件阅读器
+        SAXReader saxReader = new SAXReader();
+        
+
+        System.out.println("hello");
+    }
+}
