12e5e64258291b17e6d8d551b02ae0044dc6a119
lidf077 <lidf077@gmail.com>
2019-11-27 08:59:54 +0800

    http请求头解读

--- src/main/java/com/dongfang/javaweb/http/HttpServer.java
+++ src/main/java/com/dongfang/javaweb/http/HttpServer.java
@@ -8,20 +8,29 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- * request = GET / HTTP/1.1
+ * GET / HTTP/1.1
+ * 请求方法 资源路径 协议版本
+ *
+ * 请求头信息：请求的头部信息
  * Host: localhost:31943
- * Connection: keep-alive
+ * Connection: keep-alive 长连接
  * Cache-Control: max-age=0
  * Upgrade-Insecure-Requests: 1
+ * 用户代理：用户发送请求时使用的工具的详细信息，用户客户端的详细信息
  * User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36
  * Sec-Fetch-User: ?1
+ * 可以接收的内容类型
  * Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,
-        *Sec-Fetch-Site:cross-site
-        *Sec-Fetch-Mode:navigate
-        *Accept-Encoding:gzip,deflate,br
-        *Accept-Language:en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7
-        *Cookie:_ga=GA1.1.1172827387.1526876019
-
+ * Sec-Fetch-Site:cross-site
+ * Sec-Fetch-Mode:navigate
+ * 接收的编码格式
+ * Accept-Encoding:gzip,deflate,br
+ * Accept-Language:en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7
+ * Cookie:_ga=GA1.1.1172827387.1526876019
+ *
+ * 所有的请求头；https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
+ *
+ * GET请求没有请求体
  */
 public class HttpServer {
     public static void main(String[] args) throws IOException {
@@ -44,7 +53,6 @@ public class HttpServer {
 
             inputStream.close();
             writer.close();
-
 
 
         }
